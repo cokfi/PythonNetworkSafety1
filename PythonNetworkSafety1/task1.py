9329e627427e25a36af8fbba2a500d3e11a6b830
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-10-21 22:35:53 +0300

    rename file to task1

--- PythonNetworkSafety1/task1.py
+++ PythonNetworkSafety1/task1.py
@@ -1,4 +1,6 @@
-#
+
+# program to calculate your age in a hundred years - task1 NetworkSafety
+
 def task1():
   print("Enter your name")
   name = input()
@@ -6,6 +8,7 @@ def task1():
   age = input()
   age = int(age)+100
   print(name," you will be ",age," years old in a hundred years!" )
+
 def main():
     task1()
 
