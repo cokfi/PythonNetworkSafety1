f04f5167af1fa8eba5aebbefbf7fbb71a1cd3942
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-10-21 23:38:02 +0300

    task2 completed

--- PythonNetworkSafety1/task2.py
+++ PythonNetworkSafety1/task2.py
@@ -1,1 +1,17 @@
-# hello
+
+# python program to calculate the divisors of a number
+
+def task2():
+  print("Enter a number")
+  num = int(input())
+  divisors = []
+  for i in range(2,num+1):
+    if (num%i ==0):
+        divisors.append(i)
+  print(divisors)
+
+def main():
+    task2()
+
+if __name__ == '__main__':
+    main()
