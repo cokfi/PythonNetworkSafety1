5d4a40ab03e1c6ca6b39bb6ff44553e01134fde5
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-10-21 22:23:42 +0300

    task1 completed

--- PythonNetworkSafety1/PythonNetworkSafety1.py
+++ PythonNetworkSafety1/PythonNetworkSafety1.py
@@ -1,9 +1,13 @@
 #
 def task1():
-
-
+  print("Enter your name")
+  name = input()
+  print("Enter your age")
+  age = input()
+  age = int(age)+100
+  print(name," you will be ",age," years old in a hundred years!" )
 def main():
-
+    task1()
 
 if __name__ == '__main__':
     main()
